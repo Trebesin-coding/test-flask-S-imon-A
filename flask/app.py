a70e2de3e16e08ef995816cc123d42fc8585a169
S-imon-A <andel122@trebesin.cz>
2026-02-13 12:34:15 +0100

    pridani reseni

--- flask/app.py
+++ flask/app.py
@@ -1,17 +1,52 @@
-# Z následujících si vyber kód a sestav funkční flask aplikaci (není třeba použít vše, vyber si pouze ty části kódu, které potřebuješ)
-# Kód je funkční, pouze místo dotazníků je potřeba doplnit podle potřeby
+from flask import Flask, request, render_template, url_for, redirect
+import json
 
+app = Flask(__name__)
 
-# app = Flask(__name__)
-# from flask import Flask, render_template, request, redirect, url_for
-# @app.route("???")
-# request.args.get("???")
-# request.form.get("???")
-# print("???")
-# cursor.execute("???")
-# if request.method == "POST"
-# render_template("???", ???, ???)
+data: list[dict] = []
 
+def load_file_data() -> list[dict]:
+    loaded_data: list[dict] = []
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+    with open("data/recenze.json", "r", encoding="UTF-8") as file:
+        loaded_data = json.load(file)
+
+    return loaded_data
+
+def save_data_to_file(data_to_save: list[dict]) -> None:
+    with open("data/recenze.json", "w", encoding="UTF-8") as file:
+        json.dump(data_to_save, file, indent=4)
+
+@app.route("/")
+def vitej() -> str:
+    return render_template("vitej.html")
+
+@app.route("/form", methods=["GET", "POST"])
+def form() -> str:
+    if request.method == "POST":
+        login: str = request.form.get("login")
+        recenze: str = request.form.get("recenze")
+
+        #if recenze == "nic":
+        if len(recenze) <= 3:
+            recenze = "uživatel byl příliš líný na napsání recenze"
+        
+        recenze_data: dict = {
+            "login": login,
+            "recenze": recenze
+        }
+
+        data.append(recenze_data)
+
+        save_data_to_file(data)
+
+        return render_template("form.html", login=login, recenze=recenze)
+
+    return render_template("form.html", login="", recenze="")
+
+#tato část kódu se zpustí, pokuď je tento soubor spuštěn jako, hlavní (není jenom importován)
+if __name__ == "__main__":
+    data = load_file_data()
+
+    #tato část zpustí aplikaci, jako local development server a bude v debug módu
+    app.run(debug=True)
